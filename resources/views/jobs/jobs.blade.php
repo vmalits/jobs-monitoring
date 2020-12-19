c2b09438df0ff7eb0a45548a5bd25f43340e6c7d
Vladimir Malits <vladimir.malits@gmail.com>
2020-12-19 20:52:42 +0200

    added search jobs functionality

---
+++ resources/views/jobs/jobs.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <search-jobs></search-jobs>
+        </div>
+    </div>
+@endsection
